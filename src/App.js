104a4d37c01279706564bf1ad7aa745aa7f23cae
Hendrik Mans <hendrik@mans.de>
2022-06-07 17:16:37 +0200

    handle clicks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+
+function SplodyBox() {
+  const [damage, setDamage] = useState(0);
+
+  const handleClick = () => {
+    setDamage(damage + 1);
+  };
+
+  return (
+    <mesh onClick={handleClick}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshStandardMaterial attach="material" color="hotpink" />
-      </mesh>
+
+      <SplodyBox />
+
+      <OrbitControls />
     </Canvas>
   );
 }
