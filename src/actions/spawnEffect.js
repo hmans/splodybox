414825636c6a58efcba66ce9e4f354226b8f5b07
Hendrik Mans <hendrik@mans.de>
2022-06-07 18:09:17 +0200

    Cleanup

--- src/actions/spawnEffect.js
+++ src/actions/spawnEffect.js
@@ -2,7 +2,7 @@ import { Tag } from "miniplex";
 import { ECS } from "../store";
 
 export const spawnEffect = () => {
-  const e = ECS.world.createEntity({
+  ECS.world.createEntity({
     isEffect: Tag,
   });
 };
