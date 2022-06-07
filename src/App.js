1af3fe407dcda2fa5724be1e97d8a56144692f95
Hendrik Mans <hendrik@mans.de>
2022-06-07 20:59:42 +0200

    Tweak shadows

--- src/App.js
+++ src/App.js
@@ -20,18 +20,22 @@ function Camera() {
 function App() {
   return (
     <Canvas shadows>
-      <ambientLight />
+      <ambientLight intensity={0.5} />
+
       <pointLight
         position={[20, 10, 12]}
         castShadow
-        shadow-mapSize-width={512}
-        shadow-mapSize-height={512}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
         shadow-camera-far={50}
         shadow-camera-left={-20}
         shadow-camera-right={20}
         shadow-camera-top={20}
         shadow-camera-bottom={-20}
+        shadow-radius={10}
+        shadow-bias={-0.0001}
       />
+
       <Camera />
 
       <SplodyBoxSpawner />
